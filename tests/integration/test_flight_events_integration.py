a63218a812a9a0114d98a2c4bf85c0cf31c92c3c
Matias Vazquez <mfvazquez@fi.uba.ar>
2025-02-23 17:06:45 +0000

    test: update endpoint

--- tests/integration/test_flight_events_integration.py
+++ tests/integration/test_flight_events_integration.py
@@ -6,7 +6,7 @@ from fastapi import status
 @pytest.mark.asyncio
 async def test_real_flight_events_api(test_app: AsyncClient):
     response = await test_app.get(
-        "/search",
+        "/journeys/search",
         params={"from": "MAD", "to": "BUE", "departure_date": "2021-12-31"},
     )
     assert response.status_code == status.HTTP_200_OK
@@ -31,7 +31,7 @@ async def test_real_flight_events_api_with_unexisting_airport_destination(
     test_app: AsyncClient,
 ):
     response = await test_app.get(
-        "/search",
+        "/journeys/search",
         params={"from": "MAD", "to": "XXX", "departure_date": "2021-12-31"},
     )
     assert response.status_code == status.HTTP_404_NOT_FOUND
@@ -42,7 +42,7 @@ async def test_real_flight_events_api_with_unexisting_airport_origin(
     test_app: AsyncClient,
 ):
     response = await test_app.get(
-        "/search",
+        "/journeys/search",
         params={"from": "XXX", "to": "BUE", "departure_date": "2021-12-31"},
     )
     assert response.status_code == status.HTTP_404_NOT_FOUND
@@ -53,7 +53,7 @@ async def test_real_flight_events_api_with_existing_airports_and_no_route(
     test_app: AsyncClient,
 ):
     response = await test_app.get(
-        "/search",
+        "/journeys/search",
         params={"from": "BUE", "to": "MAD", "departure_date": "2021-12-31"},
     )
     assert response.status_code == status.HTTP_200_OK
